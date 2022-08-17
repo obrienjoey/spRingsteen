94687dd9e3aae50693185d1e51a49b93c300fcda
obrienjoey <mr.joeyob@gmail.com>
2022-08-17 23:04:03 +0100

    updated to fix CMD check

--- R/data_refresh.R
+++ R/data_refresh.R
@@ -3,7 +3,9 @@
 #' @description Checks if new data is available on the package dev version (Github).
 #' In case new data is available the function will enable the user the update the datasets
 #'
-#' @example
+#' @importFrom rlang .data
+#'
+#' @examples
 #' \dontrun{
 #'
 #' data_update()
@@ -11,25 +13,25 @@
 #' }
 update_data <- function(){
   flag <- FALSE
-
+  `%>%` <- magrittr::`%>%`
   # check if there are concert updates
   concerts_current <- spRingsteen::concerts
 
   concerts_git <- readr::read_csv("https://raw.githubusercontent.com/obrienjoey/springsteen_db/master/csv/concerts.csv") %>%
-    rename(gig_key = gig_url) %>%
-    mutate(state = case_when(nchar(sub('.*\\, ', '', location)) == 2 ~ sub('.*\\, ', '', location),
-                             TRUE ~ ''),
-           city = case_when(nchar(sub('.*\\, ', '', location)) != 2 ~ stringr::str_to_title(sub('.*\\, ', '', location)),
-                            TRUE ~ ''),
-           location = case_when(sub('.*\\, ', '', location) %in% state.abb ~ paste0(location, ', USA'),
-                                nchar(sub('.*\\, ', '', location)) == 2 ~ paste0(location, ', Canada'),
-                                TRUE ~ location),
-           country = sub('.*\\, ', '', location),
-           country = ifelse(country == 'USA', country, stringr::str_to_title(country)),
+    dplyr::rename(gig_key = .data$gig_url) %>%
+    dplyr::mutate(state = dplyr::case_when(nchar(sub('.*\\, ', '', .data$location)) == 2 ~ sub('.*\\, ', '', .data$location),
+                                           TRUE ~ ''),
+           city = dplyr::case_when(nchar(sub('.*\\, ', '', .data$location)) != 2 ~ stringr::str_to_title(sub('.*\\, ', '', .data$location)),
+                                   TRUE ~ ''),
+           location = dplyr::case_when(sub('.*\\, ', '', .data$location) %in% state.abb ~ paste0(.data$location, ', USA'),
+                                       nchar(sub('.*\\, ', '', .data$location)) == 2 ~ paste0(.data$location, ', Canada'),
+                                       TRUE ~ .data$location),
+           country = sub('.*\\, ', '', .data$location),
+           country = dplyr::if_else(.data$country == 'USA', .data$country, stringr::str_to_title(.data$country)),
            date = as.Date(date, format = '%Y-%M-%d')) %>%
-    mutate(across(where(is.character), ~ if_else(.x == '', NA_character_ ,.x)))
+    dplyr::mutate(dplyr::across(tidyselect::vars_select_helpers$where(is.character), ~ dplyr::if_else(.x == '', NA_character_ ,.x)))
 
-  if(!(identical(concerts_current, concerts_git))){
+  if(!(base::identical(concerts_current, concerts_git))){
     flag <- TRUE
   }
 
@@ -37,13 +39,13 @@ update_data <- function(){
   setlists_current <- spRingsteen::setlists
 
   setlists_git <- readr::read_csv('https://raw.githubusercontent.com/obrienjoey/springsteen_db/main/csv/setlists.csv') %>%
-    rename(gig_key = gig_url,
-           song_key = links,
-           song = songs) %>%
-    mutate(song_number = as.integer(song_number),
-           song = stringr::str_to_title(song))
+    dplyr::rename(gig_key = .data$gig_url,
+                  song_key = .data$links,
+                  song = .data$songs) %>%
+    dplyr::mutate(song_number = as.integer(.data$song_number),
+                  song = stringr::str_to_title(.data$song))
 
-  if(!(identical(setlists_current, setlists_git))){
+  if(!(base::identical(setlists_current, setlists_git))){
     flag <- TRUE
   }
 
@@ -51,19 +53,21 @@ update_data <- function(){
   songs_current <- spRingsteen::songs
 
   songs_git <- readr::read_csv('https://raw.githubusercontent.com/obrienjoey/springsteen_db/main/csv/songs.csv') %>%
-    rename(song_key = links,
-           title = titles)
+    dplyr::rename(song_key = .data$links,
+                  title = .data$titles)
 
-  if(!(identical(songs_current, songs_git))){
+  if(!(base::identical(songs_current, songs_git))){
     flag <- TRUE
   }
 
   # check if there are tour updates
-  tours_git <- readr::read_csv('https://raw.githubusercontent.com/obrienjoey/springsteen_db/main/csv/tours.csv') %>%
-    rename(gig_key = gig_url) %>%
-    filter(gig_key %in% concerts_git$gig_key)
+  tours_current <- spRingsteen::tours
 
-  if(!(identical(tours_current, tours_git))){
+  tours_git <- readr::read_csv('https://raw.githubusercontent.com/obrienjoey/springsteen_db/main/csv/tours.csv') %>%
+    dplyr::rename(gig_key = .data$gig_url) %>%
+    dplyr::filter(.data$gig_key %in% concerts_git$gig_key)
+
+  if(!(base::identical(tours_current, tours_git))){
     flag <- TRUE
   }
 
@@ -79,11 +83,11 @@ update_data <- function(){
           base::message("The data was refreshed, please restart your session to have the new data available")
         },
         error = function(e){
-          message('Caught an error!')
+          base::message('Caught an error!')
           print(e)
         },
         warning = function(w){
-          message('Caught an warning!')
+          base::message('Caught an warning!')
           print(w)
         }
 
