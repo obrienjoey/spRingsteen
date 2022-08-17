e85e149365989c0e3507d731d3285a05cb79348a
obrienjoey <mr.joeyob@gmail.com>
2022-08-17 22:01:43 +0100

    add potential to update data in dev

---
+++ R/data_refresh.R
@@ -0,0 +1,95 @@
+#' Update the Package Datasets
+#' @export
+#' @description Checks if new data is available on the package dev version (Github).
+#' In case new data is available the function will enable the user the update the datasets
+#'
+#' @example
+#' \dontrun{
+#'
+#' data_update()
+#'
+#' }
+update_data <- function(){
+  flag <- FALSE
+
+  # check if there are concert updates
+  concerts_current <- spRingsteen::concerts
+
+  concerts_git <- readr::read_csv("https://raw.githubusercontent.com/obrienjoey/springsteen_db/master/csv/concerts.csv") %>%
+    rename(gig_key = gig_url) %>%
+    mutate(state = case_when(nchar(sub('.*\\, ', '', location)) == 2 ~ sub('.*\\, ', '', location),
+                             TRUE ~ ''),
+           city = case_when(nchar(sub('.*\\, ', '', location)) != 2 ~ stringr::str_to_title(sub('.*\\, ', '', location)),
+                            TRUE ~ ''),
+           location = case_when(sub('.*\\, ', '', location) %in% state.abb ~ paste0(location, ', USA'),
+                                nchar(sub('.*\\, ', '', location)) == 2 ~ paste0(location, ', Canada'),
+                                TRUE ~ location),
+           country = sub('.*\\, ', '', location),
+           country = ifelse(country == 'USA', country, stringr::str_to_title(country)),
+           date = as.Date(date, format = '%Y-%M-%d')) %>%
+    mutate(across(where(is.character), ~ if_else(.x == '', NA_character_ ,.x)))
+
+  if(!(identical(concerts_current, concerts_git))){
+    flag <- TRUE
+  }
+
+  # check if there are setlist updates
+  setlists_current <- spRingsteen::setlists
+
+  setlists_git <- readr::read_csv('https://raw.githubusercontent.com/obrienjoey/springsteen_db/main/csv/setlists.csv') %>%
+    rename(gig_key = gig_url,
+           song_key = links,
+           song = songs) %>%
+    mutate(song_number = as.integer(song_number),
+           song = stringr::str_to_title(song))
+
+  if(!(identical(setlists_current, setlists_git))){
+    flag <- TRUE
+  }
+
+  # check if there are song updates
+  songs_current <- spRingsteen::songs
+
+  songs_git <- readr::read_csv('https://raw.githubusercontent.com/obrienjoey/springsteen_db/main/csv/songs.csv') %>%
+    rename(song_key = links,
+           title = titles)
+
+  if(!(identical(songs_current, songs_git))){
+    flag <- TRUE
+  }
+
+  # check if there are tour updates
+  tours_git <- readr::read_csv('https://raw.githubusercontent.com/obrienjoey/springsteen_db/main/csv/tours.csv') %>%
+    rename(gig_key = gig_url) %>%
+    filter(gig_key %in% concerts_git$gig_key)
+
+  if(!(identical(tours_current, tours_git))){
+    flag <- TRUE
+  }
+
+  if(flag){
+    q <- base::tolower(base::readline("Updates are available on the spRingsteen Dev version, do you want to update? n/Y"))
+
+    if(q == "y" | q == "yes"){
+
+      base::tryCatch(
+        expr = {
+          devtools::install_github("obrienjoey/spRingsteen")
+
+          base::message("The data was refreshed, please restart your session to have the new data available")
+        },
+        error = function(e){
+          message('Caught an error!')
+          print(e)
+        },
+        warning = function(w){
+          message('Caught an warning!')
+          print(w)
+        }
+
+      )
+    }
+  } else {
+    base::message("No updates are available")
+  }
+}
